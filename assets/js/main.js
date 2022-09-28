44da0c4d846f847a7e14fad017663b0877437362
Laura Montenegro <larislakis@hotmail.com>
2022-09-28 17:23:44 -0300

    deploy

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// trabalhando com javascript em um arquivo externo
+
+
+console.log(nome, "chamando variável criada com var");
+
+var nome ="Fiap";
+
+
+//função nominal - udf
+
+function escreve(titulo){
+    alert(titulo);
+}
